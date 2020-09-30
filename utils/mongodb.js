e9c5aa81ab9b035d2b0fba7c6087af11ee059a23
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-29 18:44:51 -0700

    installing mongoDB

---
+++ utils/mongodb.js
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb'
+
+import { MongoClient } from 'mongodb'
+
+let url = process.env.MONGODB_URI
+let dbName = process.env.MONGODB_DB
+
+let cachedClient = null
+let cachedDb = null
+
+if (!url) {
+    throw new Error(
+        'Please define the MONGODB_URL environment variable inside .env.local'
+    )
+}
+
+if (!dbName) {
+    throw new Error(
+        'Please define the MONGODB_DB environment variable inside .env.local'
+    )
+}
+
+export async function connectToDatabase() {
+    if (cachedClient && cachedDb) {
+        return { client: cachedClient, db: cachedDb }
+    }
+
+    const client = await MongoClient.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+
+    const db = await client.db(dbName)
+
+    cachedClient = client
+    cachedDb = db
+
+    return { client, db }
+}
