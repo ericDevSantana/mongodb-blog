c63df6980205927ab585fa2fe284834fa8e6f09f
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-12-01 19:32:10 -0800

    useSession()

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export default function Home() {
   const [session, loading] = useSession()
